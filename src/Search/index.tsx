c7dfc0ff50c755824538fd9650e483464464d6da
User Name <maithanhphat2001@gmail.com>
2023-10-02 15:47:22 +0700

    up

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
